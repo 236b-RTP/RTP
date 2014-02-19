808d0e0b6d4929c7c2d3ea92f66538f168253d04
Noranda Brown <noranda@norandabrown.com>
2014-02-18 20:33:18 -0500

    added bootstrap and welcome controller

--- config/routes.rb
+++ config/routes.rb
@@ -24,6 +24,8 @@ Rtp::Application.routes.draw do
 
   resources :users, except: [ :index ]
 
+  root 'welcome#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
