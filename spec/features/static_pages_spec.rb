37c5101d15fe1800c629310e50b023981c65fd3b
Noranda Brown <noranda@norandabrown.com>
2014-03-03 23:50:39 -0500

    updated static page views, added static page rspec tests

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -3,23 +3,41 @@ require 'spec_helper'
 describe 'Static pages' do
 
   describe 'Welcome page' do
+
     it 'should have the content "Productivity made easy"' do
       visit '/static_pages/welcome'
       expect(page).to have_content('Productivity made easy')
     end
+
+    it "should have the title 'Welcome'" do
+      visit '/static_pages/welcome'
+      expect(page).to have_title("Welcome")
+    end
   end
 
   describe "Help page" do
+
     it "should have the content 'Help'" do
       visit '/static_pages/help'
       expect(page).to have_content('Help')
     end
+
+    it "should have the title 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_title("Help")
+    end
   end
 
   describe "Settings page" do
+
     it "should have the content 'Settings'" do
       visit '/static_pages/settings'
       expect(page).to have_content('Settings')
+    end
+
+    it "should have the title 'Settings'" do
+      visit '/static_pages/settings'
+      expect(page).to have_title("Settings")
     end
   end
 end
