1ed3ad8ca6c41dad0a73adacfa653a4a9ec6b796
alex <apomata@brandeis.edu>
2014-03-07 14:43:37 -0500

    added to controllers changed some views
    added connections to models with in controllers. controllers not being
    called by views need to be called.
    changed the names of some fields so as to work with ruby
    (exchanged - for _)

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class TasksController < ApplicationController
   def index
   end
@@ -6,6 +8,19 @@ class TasksController < ApplicationController
   end
 
   def create
+    @task = Task.new(task_params)
+    if @task.save
+      flash[:success] = "Your task has been created."
+      puts "********************************************************TASK SAVED"
+    else
+      flash.now[:error] = "<ol><li>#{@task.errors.full_messages.join('</li><li>')}</li></ol>".html_safe
+      render :new
+      puts "*****************************************************TASK SAVED RETURNS FALSE"
+    end
+  end
+
+  def task_params
+     params.require(:task).permit(:title, :start_time,  :end_date, :event_details)
   end
 
   def edit
