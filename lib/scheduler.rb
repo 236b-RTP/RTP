8f198b3a5910c5eccd53f335192d04d67a7210d6
alex <apomata@brandeis.edu>
2014-03-13 14:28:22 -0400

    background scheduling
    grr

---
+++ lib/scheduler.rb
@@ -0,0 +1,16 @@
+class Scheduler
+	def initialize
+		@refered_times = Array.new(7){Array.new(8)}
+		@prefered_times = [	[ nil, nil, nil, nil, nil, nil, nil, nil,], 
+							[ nil, nil, nil, nil, nil, nil, nil, nil,], 
+							[ nil, nil, nil, nil, nil, nil, nil, nil,], 
+							[ nil, nil, nil, nil, nil, nil, nil, nil,], 
+							[ nil, nil, nil, nil, nil, nil, nil, nil,], 
+							[ nil, nil, nil, nil, nil, nil, nil, nil,],
+							[ nil, nil, nil, nil, nil, nil, nil, nil,],
+						]
+	end
+
+
+
+end
