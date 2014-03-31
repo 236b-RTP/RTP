46497c00aa6ed5186aa9467a27aea8e1d7d6a998
Luka Milekic <lmilekic@gmail.com>
2014-03-31 17:34:31 -0400

    updated pref_time_builder

--- lib/pref_time_builder.rb
+++ lib/pref_time_builder.rb
@@ -1,12 +1,12 @@
 def week_preferences (preferences)
-	if('early' = pref[:profile_type])
-		return earlybird(pref.start, pref.end)
-	elsif ('late' = pref[:profile_type])
-		return nightowl(pref.start, pref.end)
-	elsif('mix' = pref[:profile_type])
-		return default(pref.start, pref.end)
+	if('early' == pref[:profile_type])
+		return earlybird(pref[:start_time], pref[:end_time])
+	elsif ('late' == pref[:profile_type])
+		return nightowl(pref[:start_time], pref[:end_time])
+	elsif('mix' == pref[:profile_type])
+		return default(pref[:start_time], pref[:end_time])
 	end
-
+end
 
 def earlybird (rise, bed)
 	day = bed.hour - rise.hour
@@ -30,21 +30,22 @@ end
 def prefh (hour,type, day)
 	# 1 is early bird 2 is nightowl
 	if(type == 1)
-		if hour <= day/3 return 10
-		if hour> day/3 && hour <= (day/3)*2 return 7
-		if hour> (day/3)*2 return 4
+		if hour <= day/3 then return 10 end
+		if hour> day/3 && hour <= (day/3)*2 then return 7 end
+		if hour> (day/3)*2 then return 4 end
 	end
 
 	if(type == 2)
-		if hour <= day/3 return 4
-		if hour> day/3 && hour <= (day/3)*2 return 7
-		if hour> (day/3)*2 return 10
+		if hour <= day/3 then return 4 end
+		if hour> day/3 && hour <= (day/3)*2 then return 7 end
+		if hour> (day/3)*2 then return 10 end
 	end
 
 	if(type == 3)
-		if hour <= day/3 return 8
-		if hour> day/3 && hour <= (day/3)*2 return 10
-		if hour> (day/3)*2 return 8
+		if hour <= day/3 then return 8 end
+		if hour> day/3 && hour <= (day/3)*2 then return 10 end
+		if hour> (day/3)*2 then return 8 end
 	end
- 
 end
+
+person = {profile_type: 'early', start_time: DateTime.now - 7, end_time: DateTime.now + 5}
