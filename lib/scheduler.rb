1eab4d88fcd130f11178839c08e8f919aa087e1e
alex <apomata@brandeis.edu>
2014-04-01 19:46:45 -0400

    Revert "testing scheduler event insert"
    This reverts commit a5141ce506dbf9c5f72bedbcf2ba2a898c96debf.

--- lib/scheduler.rb
+++ lib/scheduler.rb
@@ -1,6 +1,6 @@
 require_relative 'day.rb'
 require_relative 'task_placer.rb'
-require_relative 'pref_time_builder.rb'
+require_relative 'pref_time_bulider.rb'
 require_relative 'utility.rb'
 
 class Scheduler
@@ -36,7 +36,7 @@ class Scheduler
 				while !best_times.empty? && scheduled == false do
 					pref_time = best_times.shift
 					if @week[d].date < best_task[:due_date]
-						scheduled = @week[d].insert(pref_time.time, change_dt(pref_time.time, (best_task[:duration]/60)), true)
+						scheduled = @week[d].insert(pref_time.time, change_dt(pref_time.time, (best_task[:duration]/60), true)
 					end
 				end
 				d+=1
@@ -52,7 +52,7 @@ class Scheduler
 			#do sme error for eadh not scheduled
 		end
 
-		return @week, couldnt_schedule
+		return @week couldnt_schedule
 
 	end
 
@@ -71,47 +71,10 @@ class Scheduler
 	end
 
 	def make_date(today, weekday)
-		return today > weekday ? DateTime.now + (7-today+weekday) : DateTime.now + (weekday-today)
+		return today > weekday ? DateTime.now +(7-today+weekday) : DateTime.now +(weekday-today)
 	end
 end
 
 
-def load_events(week, ev)
-	events = ev
-	#get events from database put in week days
-	week.each do|wday| 
-		deay_events = events.select{|ev| ev[:start_date].to_date == wday.date.to_date}
-		day_events.each do |d_e|
-			#make e a block object
-			day.insert(d_e[:start_time].hour, d_e[:end_time].hour, false)
-		end
-	end 
 
-end
-
-
-def make_date(today, weekday)
-	return today > weekday ? DateTime.now + (7-today+weekday) : DateTime.now + (weekday-today)
-end
-
-
-d = DateTime.new(2014, 3, 31, 8, 0, 0)
-
-user_pref = {profile_type: 'early', start_time: change_dt(d, 0), end_time: change_dt(d, 7)}
-
-#change to be users location time
-@today = DateTime.now
-		
-@week = Array.new(7){|e|  e = Day.new(user_pref[:start_time], user_pref[:end_time], make_date(@today.wday, e))}
-
-es = DateTime.new(2014, 4, 1, 11,0, 0)
-ee = DateTime.new(2014, 4, 1, 14,0, 0)
-events = [{start_time: es, end_time: ee}, {start_time: es + 1, end_time: ee + 1}, {start_time: es + 2, end_time: ee + 2}, 
-	{start_time: es + 3, end_time: ee + 3}, {start_time: es + 4, end_time: ee + 4}, {start_time: es + 5, end_time: ee + 5}, {start_time: es + 6, end_time: ee + 6},]
-
-load_events(@week, events)
-
-@week.each do |day|
-	puts day.filled.to_s
-end
 
