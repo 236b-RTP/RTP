0b141943796c3dd0a8864bd28150df1baa406ecc
alex <apomata@brandeis.edu>
2014-04-02 21:29:45 -0400

    Revert "working on adding to database"
    This reverts commit 7eb058aead4e8427880b062b7e6ba718a4cc28c0.

--- lib/run_scheduler.rb
+++ lib/run_scheduler.rb
@@ -1,16 +1,5 @@
-def run_scheduler
-	user_pref = {profile_type: 'early', start_time: change_dt(d, 7), end_time: change_dt(d, 18)}
-	user = current_user
-	
-	main_calendar = Scheduler.new(user_pref, user.tasks, user.events)
-	cal = main_calendar.schedule
-=begin
-	cal[0].each do |day|
-		tasks = day.filled.select{|task| task.is_task?}
-		tasks.each do |task|
-			#updates here
-		end
-	end
-=end
+user_pref = {profile_type: 'early', start_time: change_dt(d, 7), end_time: change_dt(d, 18)}
+user = current_user
 
-end			
+main_calendar = Scheduler.new(user_pref, user.tasks, user.events)
+main_calendar.schedule
