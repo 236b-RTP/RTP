209c16a422734a917d1440b939fab82110258dfe
alex <apomata@brandeis.edu>
2014-04-11 14:16:05 -0400

    added class checks to time block should not break anything

--- app/models/time_block.rb
+++ app/models/time_block.rb
@@ -2,10 +2,13 @@ class TimeBlock
   attr_accessor :t, :item
 
   def initialize(start, stop, type, item)
-    @t = { :begin => start, :end => stop }
-    @type = type
-    #task or event
-    @item = item
+  	if start.class == DateTime && stop.class == DateTime && !!type == type
+    	@t = { :begin => start, :end => stop }
+    	@type = type
+    	@item = item
+    else
+    	raise ArgumentError
+    end
   end
 
   def is_task?
