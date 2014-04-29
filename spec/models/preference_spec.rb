76a88df61e8954ad02a8269fad911365737495a5
Luka Milekic <lmilekic@gmail.com>
2014-04-29 13:56:34 -0400

    made changes to this a while ago, plan on adding more in upcoming days

--- spec/models/preference_spec.rb
+++ spec/models/preference_spec.rb
@@ -1,5 +1,26 @@
 require 'spec_helper'
 
 describe Preference do
-  pending "add some examples to (or delete) #{__FILE__}"
+	before do
+		@preference = Preference.new(profile_type: "foo", start_time: DateTime.now, end_time: DateTime.now)
+	end
+  subject { @preference }
+
+  it { should respond_to(:profile_type) }
+  it { should respond_to(:start_time) }
+  it { should respond_to(:end_time) }
+  describe 'when profile type is not present' do
+    before { @preference.profile_type = '' }
+    it { should_not be_valid }
+  end
+
+  describe 'when start_time is not present' do
+    before { @preference.start_time = nil}
+    it { should_not be_valid }
+  end
+
+  describe 'when end_time is not present' do
+    before { @preference.end_time = nil }
+    it { should_not be_valid }
+  end
 end
