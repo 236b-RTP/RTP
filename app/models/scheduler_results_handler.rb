8bc7eeda6c11ed2f2de52d6399a3caf067aa871b
alex <apomata@brandeis.edu>
2014-05-01 17:29:39 -0400

    added a handler for scheduler results

---
+++ app/models/scheduler_results_handler.rb
@@ -0,0 +1,29 @@
+class SchedulerResultsHandler
+	#allow for growth in handeling results from scheduller including week updates and past due returns
+	def initialize()
+	end
+
+	#run different scheduling schemas
+	def run_schedule(user, task = nil)
+		scheduler = Scheduler.new(user, task)
+		results = scheduler.schedule
+		#results[0] = week, [1] = couldnt schedule, [2] = past_due
+		update_tasks(results[0])
+	end
+
+	def run_schedule(user, task = nil)
+		scheduler = Scheduler.new(user, task)
+		results = scheduler.schedule_spread
+		#results[0] = week, [1] = couldnt schedule, [2] = past_due
+		update_tasks(results[0])	
+	end
+
+	#remove updating the databasae from scheduler
+	def update_tasks(week)
+		week.each do |day|
+	    tasks = day.filled.select { |task| task.is_task? }
+	    tasks.each do |block|
+	    	block.item.update_attributes(start_date: block.t[:begin], end_date: block.t[:end])
+	    end
+	end
+end
