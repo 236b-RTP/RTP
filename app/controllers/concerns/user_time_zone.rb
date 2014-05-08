715008595b067fdcd23e9f4e44c119ef887c56ef
Noranda Brown <noranda@norandabrown.com>
2014-05-07 20:59:53 -0400

    fixed bug where the Chronic gem was not grabbing the correct time zone

--- app/controllers/concerns/user_time_zone.rb
+++ app/controllers/concerns/user_time_zone.rb
@@ -4,7 +4,9 @@ module UserTimeZone
   def with_user_time_zone
     raise 'must be called with a block' unless block_given?
     Time.use_zone(current_user.preference.time_zone) do
+      Chronic.time_class = Time.zone
       yield
     end
+    Chronic.time_class = Time.zone
   end
 end
